0347f16a5e9aaef27f8a00f6d65884694bd2e952
Joris Verstegen <39861914+BigInteger28@users.noreply.github.com>
2025-01-21 15:03:01 +0100

    Create 21beurten.go

---
+++ 21beurten.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	for {
+		// Get input from the player
+		var input string
+		fmt.Print("Enter first engine depth(s): ")
+		fmt.Scanln(&input)
+
+		// Check the length of the input
+		inputLength := len(input)
+		if inputLength > 20 {
+			fmt.Println("Input must be 20 digits or less.")
+			continue
+		}
+
+		// If input length is less than 20, generate random numbers to fill
+		extraNumbers := ""
+		for i := 0; i < 20-inputLength; i++ {
+			extraNumbers += fmt.Sprintf("%d", rand.Intn(4)+1)
+		}
+
+		// Concatenate input with extra random numbers
+		result := input + extraNumbers
+
+		// Output the result
+		fmt.Println("Generated Output:", result)
+		fmt.Println()
+	}
+}
