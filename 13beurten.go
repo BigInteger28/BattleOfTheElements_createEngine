6343340d1ebe3baf3f1dd17bd9deece920c56fbc
Joris Verstegen <39861914+BigInteger28@users.noreply.github.com>
2025-01-29 23:02:15 +0100

    Update 13beurten.go

--- 13beurten.go
+++ 13beurten.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"strconv"
 )
 
 func main() {
@@ -11,29 +12,33 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	for {
-		// Get input from the player
-		var input string
+		var amount, startseed int
+		var input, extraNumbers, result string
+		fmt.Print("Start Number: ")
+		fmt.Scanln(&startseed)
+		fmt.Print("Amount of generations: ")
+		fmt.Scanln(&amount)
 		fmt.Print("Enter first engine depth(s): ")
 		fmt.Scanln(&input)
-
+		
 		// Check the length of the input
 		inputLength := len(input)
 		if inputLength > 12 {
 			fmt.Println("Input must be 12 digits or less.")
 			continue
 		}
-
-		// If input length is less than 12, generate random numbers to fill
-		extraNumbers := ""
-		for i := 0; i < 12-inputLength; i++ {
-			extraNumbers += fmt.Sprintf("%d", rand.Intn(4)+1)
-		}
-
-		// Concatenate input with extra random numbers
-		result := input + extraNumbers
-
-		// Output the result
-		fmt.Println("Generated Output:", result)
+		
+		for i := 0; i < amount; i++ {
+			// If input length is less than 12, generate random numbers to fill
+			extraNumbers = ""
+			for j := 0; j < 12-inputLength; j++ {
+				extraNumbers += fmt.Sprintf("%d", rand.Intn(4)+1)
+			}
+			// Concatenate input with extra random numbers
+			result = "x" + strconv.Itoa(startseed) + ":1:" + input + extraNumbers
+			fmt.Println(result)
+			startseed++
+		}		
 		fmt.Println()
 	}
 }
